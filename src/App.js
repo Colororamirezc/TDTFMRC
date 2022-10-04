99b5223fca5678020bd22aad322cc20952ae937d
Daniel <dramosv@smu.cl>
2022-10-03 22:15:30 -0300

    feat: create table

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import React from "react";
-import Navbar from "./components/navbar/Aside";
+import React, { useState } from 'react';
+import Navbar from './components/navbar/Navbar';
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import { Route, Routes } from "react-router-dom";
+
+import { drawerWidth, Main, DrawerHeader } from './components/navbar/styles/styles';
+import StaffPresent from './screens/students/StaffPresent';
 
 const App = () => {
+  const [open, setOpen] = useState(false);
   return (
-    <>
-      <Navbar />
-    </>
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <Navbar open={open} setOpen={setOpen} drawerWidth={drawerWidth} />
+      <Main open={open}>
+        <DrawerHeader />
+        <Routes>
+          <Route path='/parte' element={<StaffPresent />} />
+        </Routes>
+      </Main>
+    </Box>
   );
 };
 
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,33 +1,78 @@
-import React from "react";
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
-import IconButton from "@mui/material/IconButton";
-import MenuIcon from "@mui/icons-material/Menu";
+import React from 'react';
+import { useTheme } from '@mui/material/styles';
+import Drawer from '@mui/material/Drawer';
+import Toolbar from '@mui/material/Toolbar';
+import List from '@mui/material/List';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import { AppBar, DrawerHeader, drawerWidth } from './styles/styles';
 
-const Navbar = () => {
+const Navbar = ({ open, setOpen }) => {
+  const theme = useTheme();
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
+    <>
+      <AppBar position='fixed' open={open}>
         <Toolbar>
           <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
+            color='inherit'
+            aria-label='open drawer'
+            onClick={handleDrawerOpen}
+            edge='start'
+            sx={{ mr: 2, ...(open && { display: 'none' }) }}
           >
             <MenuIcon />
           </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
+          <Typography variant='h6' noWrap component='div'>
+            Parte alimentación
           </Typography>
-          <Button color="inherit">Login</Button>
         </Toolbar>
       </AppBar>
-    </Box>
+      <Drawer
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          '& .MuiDrawer-paper': {
+            width: drawerWidth,
+            boxSizing: 'border-box',
+          },
+        }}
+        variant='persistent'
+        anchor='left'
+        open={open}
+      >
+        <DrawerHeader>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+          </IconButton>
+        </DrawerHeader>
+        <List>
+          <ListItem key={'Ingrese parte'} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <InboxIcon />
+              </ListItemIcon>
+              <ListItemText primary={'Ingrese parte'} />
+            </ListItemButton>
+          </ListItem>
+        </List>
+      </Drawer>
+    </>
   );
 };
 
