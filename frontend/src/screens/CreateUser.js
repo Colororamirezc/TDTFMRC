41c7b469cc2565755a217f6cae627b13628181ff
colororamirezc <colororamirez@gmail.com>
2022-10-25 14:09:51 -0300

    feat: CreateUserMod

--- frontend/src/screens/CreateUser.js
+++ frontend/src/screens/CreateUser.js
@@ -1,70 +1,70 @@
-import React, { useState } from 'react';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import Select from '@mui/material/Select';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import React, { useState } from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import Select from "@mui/material/Select";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 const theme = createTheme();
 
 const initialRegister = {
-  name: '',
-  lastName: '',
-  rut: '',
-  grade: '',
-  specialty: '',
+  name: "",
+  lastName: "",
+  rut: "",
+  grade: "",
+  specialty: "",
   administrativePermission: 0,
   FLA: 0,
-  password: '',
+  password: "",
 };
 
 const CreateUser = () => {
   const [values, setValues] = useState(initialRegister);
 
-  const handleChange = e => {
+  const handleChange = (e) => {
     setValues({ ...values, [e.target.name]: e.target.value });
   };
 
-  const handleSubmit = e => {
+  const handleSubmit = (e) => {
     e.preventDefault();
   };
 
   return (
     <ThemeProvider theme={theme}>
-      <Container component='main' maxWidth='xs'>
+      <Container component="main" maxWidth="xs">
         <CssBaseline />
         <Box
           sx={{
             marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
           }}
         >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
             <LockOutlinedIcon />
           </Avatar>
-          <Typography component='h1' variant='h5'>
+          <Typography component="h1" variant="h5">
             Regístrate Aquí
           </Typography>
-          <Box component='form' noValidate sx={{ mt: 3 }}>
+          <Box component="form" noValidate sx={{ mt: 3 }}>
             <Grid container spacing={2}>
               <Grid item xs={12} sm={6}>
                 <TextField
-                  autoComplete='given-name'
+                  autoComplete="given-name"
                   name={initialRegister.name}
                   required
                   fullWidth
-                  label='Nombre'
+                  label="Nombre"
                   autoFocus
                   id={initialRegister.name}
                   value={initialRegister.name}
@@ -76,35 +76,50 @@ const CreateUser = () => {
                   required
                   fullWidth
                   id={initialRegister.lastName}
-                  label='Apellidos'
+                  label="Apellidos"
                   name={initialRegister.lastName}
-                  autoComplete='family-name'
+                  autoComplete="family-name"
                   value={initialRegister.lastName}
                   onChange={handleChange}
                 />
               </Grid>
               <Grid item xs={12}>
-                <TextField required fullWidth id='rut' label='RUT' name='rut' autoComplete='rut' />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField required fullWidth name='grade' label='Grado' type='grade' id='grade' autoComplete='grade' />
+                <TextField
+                  required
+                  fullWidth
+                  id={initialRegister.rut}
+                  label="RUT"
+                  name={initialRegister.rut}
+                  autoComplete={initialRegister.rut}
+                />
               </Grid>
               <Grid item xs={12}>
                 <TextField
                   required
                   fullWidth
-                  name='specialty'
-                  label='Especialidad'
-                  type='specialty'
-                  id='specialty'
-                  autoComplete='specialty'
+                  name={initialRegister.grade}
+                  label="Grado"
+                  type={initialRegister.grade}
+                  id={initialRegister.grade}
+                  autoComplete={initialRegister.grade}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name={initialRegister.specialty}
+                  label="Especialidad"
+                  type={initialRegister.specialty}
+                  id={initialRegister.specialty}
+                  autoComplete={initialRegister.specialty}
                 />
               </Grid>
               <Grid item xs={12} sm={6} md={12}>
-                <InputLabel id='228'>Días de 228</InputLabel>
+                <InputLabel id="228">Días de 228</InputLabel>
                 <Select
                   id={initialRegister.administrativePermission}
-                  labelId='Días de 228'
+                  labelId="Días de 228"
                   fullWidth
                   name={initialRegister.administrativePermission}
                   required
@@ -127,37 +142,48 @@ const CreateUser = () => {
                 </Select>
               </Grid>
               <Grid item xs={12} sm={6} md={12}>
-                <TextField required fullWidth id='fla' label='FLA Disponibles' name='fla' />
-              </Grid>
-              <Grid item xs={12}>
                 <TextField
                   required
                   fullWidth
-                  name='password'
-                  label='Contraseña'
-                  type='password'
-                  id='password'
-                  autoComplete='new-password'
+                  id={initialRegister.FLA}
+                  label="FLA Disponibles"
+                  name={initialRegister.FLA}
                 />
               </Grid>
               <Grid item xs={12}>
                 <TextField
                   required
                   fullWidth
-                  name='password2'
-                  label='Repita la contraseña'
-                  type='password2'
-                  id='password2'
-                  autoComplete='new-password'
+                  name={initialRegister.password}
+                  label="Contraseña"
+                  type={initialRegister.password}
+                  id={initialRegister.password}
+                  autoComplete={initialRegister.password}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name={initialRegister.password}
+                  label="Repita la contraseña"
+                  type={initialRegister.password}
+                  id={initialRegister.password}
+                  autoComplete={initialRegister.password}
                 />
               </Grid>
             </Grid>
-            <Button type='submit' fullWidth variant='contained' sx={{ mt: 3, mb: 2 }}>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
               Regístrate
             </Button>
-            <Grid container justifyContent='flex-end'>
+            <Grid container justifyContent="flex-end">
               <Grid item>
-                <Link href='#' variant='body2'>
+                <Link href="#" variant="body2">
                   Ya tienes una cuenta? Ingresa Aquí
                 </Link>
               </Grid>
